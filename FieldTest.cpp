814ebef5b40e34270e49eeb623434b850a2369f7
Amber Miller <amiller94@mail.csuchico.edu>
2016-09-26 11:59:44 -0700

    added few tests

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 #include <iostream>
+#include <string>
 #include "Field.h"
  
 class FieldTest : public ::testing::Test
@@ -21,4 +22,25 @@ TEST(FieldTest, placeMineInBounds)
 	
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+}
+
+TEST(FieldTest, fieldSizeOutOfBounds)
+{
+  	Field minefield;
+	bool caught = false;
+	try{
+	   minefield.get(11,11);
+	}
+	catch(int err){
+	  caught = true;
+	}
+	ASSERT_TRUE(caught);
+}
+
+TEST(FieldTest, checkAdjecent )
+{
+  	Field minefield;
+	minefield.placeMine(4,5);
+	minefield.revealAdjacent(4,4);
+	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5));
 }
